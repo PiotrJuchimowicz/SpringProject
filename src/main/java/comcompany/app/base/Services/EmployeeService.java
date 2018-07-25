604067197f0183d50c36bd52b1aec87319bb8929
Piotr Juchimowicz <piotrjuchimowicz@gmail.com>
2018-07-26 00:04:14 +0200

    added some abstract method to services

--- src/main/java/comcompany/app/base/Services/EmployeeService.java
+++ src/main/java/comcompany/app/base/Services/EmployeeService.java
@@ -1,8 +1,21 @@
 package comcompany.app.base.Services;
 
+import comcompany.app.base.Models.Department;
 import comcompany.app.base.Models.Employee;
+import comcompany.app.base.Models.Position;
+import comcompany.app.base.Models.Task;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 public interface EmployeeService extends GenericService<Employee> {
+    List<Employee> findEmployeesByDepartment(Department department);
+
+    List<Employee> findEmployeesByPosition(Position position);
+
+    List<Employee> findEmployeesBySalaryBetween(double lowerLimit, double upperLimit);
+
+    List<Employee> findEmployeesWorkingOnTask(Task task);
 }
--- src/main/java/comcompany/app/base/Services/TaskService.java
+++ src/main/java/comcompany/app/base/Services/TaskService.java
@@ -1,9 +1,19 @@
 package comcompany.app.base.Services;
 
+import comcompany.app.base.Models.Employee;
+import comcompany.app.base.Models.Priority;
 import comcompany.app.base.Models.Task;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
+
 
 public interface TaskService extends GenericService<Task> {
+    List<Task> findTasksInTimeRange(LocalDate lowerRange, LocalDate upperRange);
+    List<Task> findByPriority(Priority priority);
+    List<Task> findTasksRelatedWithEmployee( Employee employee);
+
 }
