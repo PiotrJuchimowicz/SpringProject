7b295aab2c27c7fbb1b46c694585bfc77c7f9960
Piotr Juchimowicz <piotrjuchimowicz@gmail.com>
2018-08-24 01:54:23 +0200

    commit for work on laptop

--- src/main/java/comcompany/app/MvcConfig.java
+++ src/main/java/comcompany/app/MvcConfig.java
@@ -1,11 +1,13 @@
 package comcompany.app;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableAutoConfiguration
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
     @Override
