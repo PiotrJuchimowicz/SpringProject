9a48e523442011b1484317956dc6fb95f9f1e22f
Piotr Juchimowicz <piotrjuchimowicz@gmail.com>
2018-09-13 01:11:30 +0200

    added some features to AdminController and changed some views

--- src/main/java/comcompany/app/base/Controllers/AdminController.java
+++ src/main/java/comcompany/app/base/Controllers/AdminController.java
@@ -13,6 +13,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.lang.reflect.Field;
+import java.util.LinkedList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
@@ -21,14 +26,12 @@ public class AdminController {
     private EmployeeService employeeService;
 
     @Autowired
-    public void  setEmployeeService(EmployeeService employeeService)
-    {
-        this.employeeService=employeeService;
+    public void setEmployeeService(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     @RequestMapping(value = "/boss/menu")
-    public String management()
-    {
+    public String management() {
         return "admin/boss/menu";
     }
 
@@ -49,14 +52,45 @@ public class AdminController {
         employeeService.create(boss);
         return "admin/boss/menu";
     }
-    @RequestMapping(value = "/index",method = RequestMethod.GET)
-    public String index(Model model)
-    {
+
+    @RequestMapping(value = "/boss/searchingCriteria", method = RequestMethod.GET)
+    public String showSearchingCriteria() {
+        return "admin/boss/searchingCriteria";
+    }
+
+    @RequestMapping(value = "/boss/showAll", method = RequestMethod.GET)
+    public String showAll(Model model) {
+        List<Employee> allEmployees = employeeService.findEmployeesByPosition(Position.BOSS);
+        List<String> fieldsListToString = getFieldsToView();
+        model.addAttribute("employees", allEmployees);
+        model.addAttribute("fields", fieldsListToString);
+        return "/admin/boss/showAll";
+    }
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User user = (User) authentication.getPrincipal();
-        String email= user.getUsername();
+        String email = user.getUsername();
         Employee admin = employeeService.findEmployeesByEmail(email);
-        model.addAttribute("admin",admin);
+        model.addAttribute("admin", admin);
         return "admin/index";
+    }
+
+    private List<String> getFieldsToView() {
+        Field[] fields = employeeService.getAllFields(Employee.class);
+        List<String> fieldsListToString = new LinkedList<>();
+        for (Field field : fields) {
+            if (!((field.getName().equals("department")) || field.getName().equals("tasks") || field.getName().equals("id") || field.getName().equals("sentMessages") || field.getName().equals("receivedMessages"))) {
+                String fieldToString = field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1).toLowerCase();
+                //conversion from Email to E-mail
+                if (fieldToString.equals("Email"))
+                    fieldToString = "E-mail";
+                if(fieldToString.equals("Password")||fieldToString.equals("Enabled"))
+                    continue;
+                fieldsListToString.add(fieldToString);
+            }
+        }
+        return fieldsListToString;
     }
 }
